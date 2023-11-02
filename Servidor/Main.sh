dad234d464f1f2880ac5e8052c99ffff53df77fc
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-11-01 22:13:20 -0300

    Ajustes e arquivos n

--- Servidor/Main.sh
+++ Servidor/Main.sh
@@ -2,14 +2,14 @@
 
 # Para a distribuição dos arquivos e a verificação do seu sucesso por parte da máquina central, devemos utilizar os ip's das máquinas em um vetor, bem como um vetor de arquivos. {Naming}
 ip_computadores=("192.168.0.57" "192.168.0.58" "192.168.0.59")
-pdbs=("teste.ent" "teste2.ent" "teste3.ent")
+pdbs=("pdb1h6n.ent" "pdb2h6n.ent" "pdb3h6n.ent" "pdb4h6n.ent" "pdb5h6n.ent" "pdb6h6n.ent")
 
 # Para a coordenação dos processos, criamos dois arquivos .csv cujo o propósito é a identificação do estado de uma máquina e de um arquivo. A máquina poderá estar livre ou ocupada, enquanto o arquivo pode estar como processado ou não. {Coordenação}
 arquivo_csv_1="estado_maquina.csv"
 arquivo_csv_2="estado_arquivo.csv"
 
 # Inicialização dos arquivos
-echo "IP,Estado,Arquivo" > "$arquivo_csv_1"
+echo "IP,Estado,Arquivo,Comunicação" > "$arquivo_csv_1"
 for ip in "${ip_computadores[@]}"; do
   echo "$ip,Livre" >> "$arquivo_csv_1"
 done
@@ -26,16 +26,22 @@ ip_livre(){
     [[ "$estado" == "Livre" ]]
 }
 
+indice_arquivo=0
+
 # Distribuição de arquivos entre as máquinas {Coordenação}
-for pdb in "${pdbs[@]}"; do
+while [ ${#pdbs[0]} -gt 0 ]; do
     for ip in "${ip_computadores[@]}"; do
         if ip_livre "$ip"; then
             sed -i "s/$ip,Livre/$ip,Ocupado,$pdb/" "$arquivo_csv_1" # Substituição do valor de estado do ip
-            sed -i "s/$pdb,0/$pdb,1/" "$arquivo_csv_2" # Substituição do valor de estado do arquivo para processando
+            sed -i "s/$pdb,0/$pdb,1,$ip/" "$arquivo_csv_2" # Substituição do valor de estado do arquivo para processando
 
+            pdb="${pdbs[$indice_arquivo]}"
             echo "Enviando o pdb:$pdb para a máquina de ip:$ip"
+    
             # Para que o computador realize sua tarefa este deverá ter uma porta aberta para receber o arquivo da central {Comunicação}
-            nc "$ip" 9998 < "$pdb"
+
+            nc "$ip" 9998 -q 2 < "$indice_arquivo" # Envio da porta de retorno
+            nc "$ip" 9998 -q 10 < "$pdb" # Interrupção do socket e envio do pdb {Segurança}
 
             # O processos de monitoramento será em segundo plano  {Processos}
             ( 
@@ -43,27 +49,32 @@ for pdb in "${pdbs[@]}"; do
                 timeout=1200 # Limite de tempo para o processo {Tolerância a falhas}
                 resposta=""
                 while [ "$timeout" -gt 0 ]; do
-                    if [ -z "$resposta" ]; do
-                        # É possível utilizar ssh {Segurança}
-                        resposta=$(nc -l -p 9998)
+                    resposta=$(nc -l -p $indice_arquivo)
+                    if [ -z "$resposta" ]; then
                         sleep 1
                         timeout=$((timeout - 1))
+                    elif [ ! "$resposta" = "Finalizado" ]; then
+                        timeout=1200 # Reset de tempo
+                        sed -i "s/$ip,Ocupado,$pdb/$ip,Ocupado,$pdb,$resposta/" "$arquivo_csv_1"
                     else
                         break
                     fi
                 done
 
                 # Com a conclusão do processo é necessário alterar os arquivos .csv
-                if [ ! -z "$reposta" ]; then
+                if [ "$timeout" -eq 0 ]; then
                     echo "Arquivo não concluído, devolvendo $pdb ao vetor 'pdbs'"
-                    pdbs=("$pdb" "${pdb[@]}") # O arquivo retorna ao vetor de pdbs para que seja novamente processado {Replicação}
+                    pdbs=("$pdb" "${pdbs[@]}") # O arquivo retorna ao vetor de pdbs para que seja novamente processado {Replicação}
                     sed -i "s/$pdb,1/$pdb,0/" "$arquivo_csv_2"
                 else 
+                    unset pdbs[$indice_arquivo]
                     sed -i "s/$ip,Ocupado/$ip,Livre,/" "$arquivo_csv_1"
                     sed -i "s/$pdb,1/$pdb,2/" "$arquivo_csv_2"
                 fi
                 
-             ) &
+            ) &
+
+            ((indice_arquivo++))
 
             break # Interrompe o primeiro loop externo
         fi
