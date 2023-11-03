974eed98fde7d25f13f5fd060d5de09212dfcce4
Gabriel Siqueira <109818266+GabrielSiqueira1@users.noreply.github.com>
2023-11-03 11:59:52 -0300

    Servidor

--- Servidor/Main.sh
+++ Servidor/Main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Para a distribuição dos arquivos e a verificação do seu sucesso por parte da máquina central, devemos utilizar os ip's das máquinas em um vetor, bem como um vetor de arquivos. {Naming}
-ip_computadores=("192.168.0.113")
-pdbs=("pdb1h6n.ent" "pdb2h6n.ent")
+ip_computadores=("192.168.0.57" "192.168.0.10")
+pdbs=("pdb1h6n.ent" "pdb2h6n.ent" "pdb3h6n.ent" "pdb5h6n.ent" "pdb6h6n.ent")
 
 # Para a coordenação dos processos, criamos dois arquivos .csv cujo o propósito é a identificação do estado de uma máquina e de um arquivo. A máquina poderá estar livre ou ocupada, enquanto o arquivo pode estar como processado ou não. {Coordenação}
 arquivo_csv_1="estado_maquina.csv"
@@ -47,50 +47,82 @@ while ! arquivos_concluidos; do
                 fi
             done
 
-            sed -i "s/$ip,Livre/$ip,Ocupado,$pdb/" "$arquivo_csv_1" # Substituição do valor de estado do ip
-            sed -i "s/$pdb,0/$pdb,1,$ip/" "$arquivo_csv_2" # Substituição do valor de estado do arquivo para processando
-
             pdb="${pdbs[$indice_arquivo]}"
             echo "Enviando o pdb:$pdb para a máquina de ip:$ip"
     	    indice_arquivo=$((indice_arquivo + 1))
             # Para que o computador realize sua tarefa este deverá ter uma porta aberta para receber o arquivo da central {Comunicação}
 
-            echo "$indice_arquivo" | nc -q 2 "$ip" 9998 # Envio da porta de retorno
-            nc "$ip" 9998 -q 10 < "$pdb" # Interrupção do socket e envio do pdb {Segurança}
+	    sleep 1
+            echo "$indice_arquivo" | nc -q 10 "$ip" 9998 # Envio da porta de retorno
+            if [ $? -eq 0 ]; then
+            	sleep 1
+            	nc "$ip" 9998 -q 10 < "$pdb" # Interrupção do socket e envio do pdb {Segurança}
+            	echo "Enviado"
+            
+           	sed -i "s/$ip,Livre/$ip,Ocupado,$pdb/" "$arquivo_csv_1" # Substituição do valor de estado do ip
+            	sed -i "s/$pdb,0/$pdb,1,$ip/" "$arquivo_csv_2" # Substituição do valor de estado do arquivo para processando
+            	
+            	# O processos de monitoramento será em segundo plano  {Processos}
+		    ( 
 
-            echo "Enviado"
-            # O processos de monitoramento será em segundo plano  {Processos}
-            ( 
+		        timeout=1200 # Limite de tempo para o processo {Tolerância a falhas}
+		        resposta=""
+		        
+		        while [ "$timeout" -gt 0 ]; do
+			    echo "$timeout" > "timeout_$pdb.txt"
+		            resposta=$(timeout 1 nc -l -p $indice_arquivo)
+		            if [ -z "$resposta" ]; then
+		                sleep 1
+		                timeout=$((timeout - 1))
+		            elif [ ! "$resposta" = "Finalizado" ]; then
+		                timeout=1200 # Reset de tempo
+				sleep 1
+		                sed -i "s/$ip,Ocupado,$pdb/$ip,Ocupado,$pdb,$resposta/" "$arquivo_csv_1"
+		            else
+		            	sed -i "s/$ip,Ocupado,$pdb/$ip,Ocupado,$pdb,$resposta/" "$arquivo_csv_1"
+		            	while true; do
+		            		nc -l -p $indice_arquivo > "arquivo_$ip-$pdb.txt"
+		            		if [ $? -eq 0 ]; then
+				    		sleep 1
+				    		break
+				    	fi
+				done
+		                break
+		            fi
+		        done
 
-                timeout=600 # Limite de tempo para o processo {Tolerância a falhas}
-                resposta=""
-                
-                while [ "$timeout" -gt 0 ]; do
-		            echo "$timeout"
-                    resposta=$(timeout 1 nc -l -p $indice_arquivo) 
-                    if [ -z "$resposta" ]; then
-                        sleep 1
-                        timeout=$((timeout - 1))
-                    elif [ ! "$resposta" = "Finalizado" ]; then
-                        timeout=600 # Reset de tempo
-                        sed -i "s/$ip,Ocupado,$pdb.*/$ip,Ocupado,$pdb,$resposta/" "$arquivo_csv_1"
-                    else
-                        break
-                    fi
-                done
-
-                nc -l -p $indice_arquivo > "arquivo_$ip-$pdb.txt"
-
-                # Com a conclusão do processo é necessário alterar os arquivos .csv
-                if [ "$timeout" -eq 0 ]; then
-                    echo "Arquivo não concluído, devolvendo $pdb ao vetor 'pdbs'"
-                    sed -i "s/$pdb,1/$pdb,0/" "$arquivo_csv_2"
-                else 
-                    sed -i "s/$ip,Ocupado.*/$ip,Livre,/" "$arquivo_csv_1"
-                    sed -i "s/$pdb,1/$pdb,2/" "$arquivo_csv_2"
-                fi
-                
-            ) &
+			sleep 1
+		        # Com a conclusão do processo é necessário alterar os arquivos .csv
+		        if [ "$timeout" -eq 0 ]; then
+		            echo "Arquivo não concluído"
+		            while true; do
+		            	sed -i "s/$pdb,1/$pdb,0/" "$arquivo_csv_2"
+		            	if [ $? -eq 0 ]; then
+		            		sleep 1
+		            		break
+		            	fi
+		       	    done
+		        else 
+		        	while true; do
+				    	sed -i "s/$pdb,1/$pdb,2/" "$arquivo_csv_2"
+				    	if [ $? -eq 0 ]; then
+				    		sleep 1
+				    		break
+				    	fi
+		       	    	done
+		       	    	while true; do
+				    	sed -i "s/$ip,Ocupado/$ip,Livre,/" "$arquivo_csv_1"
+				    	if [ $? -eq 0 ]; then
+				    		sleep 1
+				    		break
+				    	fi
+		       	    	done
+		        fi
+		        
+		    ) &
+            fi
+            
+            
 
             break # Interrompe o primeiro loop externo
         fi
