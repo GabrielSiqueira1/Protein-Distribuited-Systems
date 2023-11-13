dcf29c5fb8c8b7ed4c970dabeb73d7f0f3cee442
Gabriel Siqueira <109818266+GabrielSiqueira1@users.noreply.github.com>
2023-11-13 07:13:04 -0300

    Portas

--- Cliente/Main.sh
+++ Cliente/Main.sh
@@ -83,7 +83,7 @@ while true; do
   else # Envio para outro servidor
     while true; do
       menor_valor_das_$menor_distancia >> "arquivo_$(hostname -I)-$porta_retorno.txt"
-      nc "$ip_server_2" $porta_retorno -q 5 < "arquivo_$(hostname -I)-$porta_retorno.txt"
+      nc "$ip_server_2" 10000 -q 5 < "arquivo_$(hostname -I)-$porta_retorno.txt"
       if [ $? -eq 0 ]; then
           break
       fi 
