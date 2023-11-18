35f8f0238434ba9ae166c201426a7977454ba256
Gabriel Siqueira <109818266+GabrielSiqueira1@users.noreply.github.com>
2023-11-17 21:26:29 -0300

    Update Update.sh

--- Servidor/Update.sh
+++ Servidor/Update.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-# Nome dos arquivos CSV
-arquivo_csv="estado_maquina.csv"
+arquivo_csv_1="estado_maquina.csv"
 arquivo_csv_2="estado_arquivo.csv"
 
 for file in arquivo-*.txt; do
-    ip=$(echo "$file" | cut -d'-' -f2 | cut -d'.' -f1-4)
-
-    if grep -q "$ip" "$arquivo_csv"; then
-        linha=$(grep "$ip" "$arquivo_csv")
-
-        filename=$(echo "$linha" | cut -d',' -f3)
-
-        status=$(echo "$linha" | cut -d',' -f2)
-
-        if [ "$status" = "Ocupado" ]; then
-            sed -i "s/$ip,Ocupado,/$ip,Livre,/" "$arquivo_csv"
-        fi
-
-        if grep -q "$filename" "$arquivo_csv_2"; then
-            sed -i "s/$filename,1,/$filename,2,/" "$arquivo_csv_2"
-        fi
-    fi
+    ip=$(echo $file | awk -F'-' '{print $2}')
+    nome_arquivo=$(echo $file | awk -F'-' '{print $3}')
+    
+    awk -v ip="$ip" -v nome_arquivo="$nome_arquivo" -F',' '$1 == ip && $3 == nome_arquivo { $2 = "NovoEstado"; print }' $arquivo_csv_1 > temp1.csv
+    mv temp1.csv $arquivo_csv_1
+    
+    awk -v nome_arquivo="$nome_arquivo" -F',' '$1 == nome_arquivo { $2 = "NovoEstado"; print }' $arquivo_csv_2 > temp2.csv
+    mv temp2.csv $arquivo_csv_2
 done
 
 echo "Operação concluída."
