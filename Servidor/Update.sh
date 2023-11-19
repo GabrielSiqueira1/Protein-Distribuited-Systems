d214ca200b9b69ee8450c5444085a080d254bc65
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-11-18 22:16:46 -0300

    Update.sh

--- Servidor/Update.sh
+++ Servidor/Update.sh
@@ -7,10 +7,10 @@ for file in arquivo-*.txt; do
     ip=$(echo $file | awk -F'-' '{print $2}')
     nome_arquivo=$(echo $file | awk -F'-' '{print $3}')
     
-    awk -v ip="$ip" -v nome_arquivo="$nome_arquivo" -F',' '$1 == ip && $3 == nome_arquivo { $2 = "NovoEstado"; print }' $arquivo_csv_1 > temp1.csv
+    awk -v ip="$ip" -v nome_arquivo="$nome_arquivo" -F',' '$1 == ip && $3 == nome_arquivo { $2 = "Livre"; print }' $arquivo_csv_1 > temp1.csv
     mv temp1.csv $arquivo_csv_1
     
-    awk -v nome_arquivo="$nome_arquivo" -F',' '$1 == nome_arquivo { $2 = "NovoEstado"; print }' $arquivo_csv_2 > temp2.csv
+    awk -v nome_arquivo="$nome_arquivo" -F',' '$1 == nome_arquivo { $2 = "2"; print }' $arquivo_csv_2 > temp2.csv
     mv temp2.csv $arquivo_csv_2
 done
 
